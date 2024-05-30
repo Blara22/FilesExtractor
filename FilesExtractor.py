99c1a5c6dd492ab61518f2cd91372939b7b7098a
Brenda Lara <blara.dev@gmail.com>
2024-05-30 11:10:32 -0700

    Extraer archivos zip internos

--- FilesExtractor.py
+++ FilesExtractor.py
@@ -1,13 +1,14 @@
 if __name__ == '__main__':
     import os
     import shutil
-    from zipfile import ZipFile
+    from zipfile import ZipFile, is_zipfile
     from dotenv import load_dotenv
 
     load_dotenv()
 
     directorio = input("Ruta del archivo: ")
 
+    #Extraer todos los archivos
     with ZipFile(directorio, 'r') as zipObj:
         # Extract all the contents of zip file in different directory
         directorio = os.path.join(os.path.dirname(directorio), os.path.splitext(os.path.basename(directorio))[0])
@@ -16,33 +17,35 @@ if __name__ == '__main__':
         zipObj.extractall(directorio)
 
     materia = input("A dónde se van a extraer: "
-                    "\n1. Programación 2 TM"
-                    "\n2. Programación 2 TV"
-                    "\n3. Estructura de Datos TM"
-                    "\n4. Estructura de Datos TV"
+                    "\n1. Programación 1 TM"
+                    "\n2. Programación 1 TV"
+                    "\n3. Programación 3 TM"
                     "\nOpción: ")
 
     if materia == "1":
-        carpeta = "Programación 2\\TM"
+        carpeta = "Programación 1\\TM"
     elif materia == "2":
-        carpeta = "Programación 2\\TV"
+        carpeta = "Programación 1\\TV"
     elif materia == "3":
-        carpeta = "Estructura de Datos\\TM"
-    elif materia == "4":
-        carpeta = "Estructura de Datos\\TV"
+        carpeta = "Programación 3"
 
     print("Extrayendo...")
 
+    #Ontener los subdirectorios que existen de lo extraído
     subdirectorios = [f.path for f in os.scandir(directorio) if f.is_dir()]
     nuevoDirectorio = os.path.join(os.getenv('MAIN_FILE_DIRECTORY'), carpeta)
     nombreDelDirectorioOriginal = os.path.split(directorio)[1]
 
+    #Si no existe el directorio, crearlo.
     if not os.path.exists(os.path.join(nuevoDirectorio, nombreDelDirectorioOriginal)):
         os.makedirs(os.path.join(nuevoDirectorio, nombreDelDirectorioOriginal))
 
+    #Por cada carpeta descargada se van a copiar los archivos con un nuevo nombre
     for subdirectorio in subdirectorios:
+        #El indice es por si se repite el nombre de algún archivo, al final se coloca un (0)(1) etc.
         index = 0
         for file in os.listdir(subdirectorio):
+
             print("Copiando " + os.path.split(subdirectorio)[1] + "...")
             nombreArchivoNuevo = os.path.basename(subdirectorio).split("_")[0] + " " + os.path.splitext(file)[0]
             extension = os.path.splitext(file)[1]
@@ -54,6 +57,18 @@ if __name__ == '__main__':
             newFolder = os.path.join(nuevoDirectorio+"\\"+nombreDelDirectorioOriginal, nombreArchivoNuevo)
             shutil.move(os.path.join(directorio, subdirectorio, file), newFolder)
             #index = index + 1
+
+
+            #Si lo que se copió es un archivo zip, se extrae en el directorio que le corresponde.
+            if file.endswith('.zip'):
+                with ZipFile(newFolder, 'r') as zipObj:
+                    # Extract all the contents of zip file in different directory
+                    dirSegundoZip = os.path.join(os.path.dirname(newFolder), os.path.splitext(os.path.basename(newFolder))[0])
+                    print(dirSegundoZip)
+                    if not os.path.exists(dirSegundoZip):
+                        os.makedirs(dirSegundoZip)
+                    zipObj.extractall(dirSegundoZip)
+                os.remove(newFolder)
 
     shutil.rmtree(directorio)
 
